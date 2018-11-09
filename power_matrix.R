b28ddb290baa9d215721ee0ccac12e19f3f772c7
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-11-09 20:01:55 +0100

    some fixes

--- power_matrix.R
+++ power_matrix.R
@@ -8,5 +8,5 @@ rho <- as.numeric(args[3])
 phi0 <- as.numeric(args[4])
 f <- as.numeric(args[5])
 
-result<-optimize_parameter(fun=est_power,opt1="n", opt2="lambda0",opt1Value=c(5,10,15,20,25,30,45,50,55,60,65,70), opt2Value=c(1:5,10,20), m=m, m1=m1, rho=rho, phi0=phi0, f=f)
+result<-optimize_parameter(fun=est_power,opt1="n", opt2="lambda0",opt1Value=c(5,10,15,20,25,30,45,50,55,60,65,70), opt2Value=c(1:5,10,20), m=m, m1=m1, rho=rho, phi0=phi0, alpha=f)
 print(result)
--- script.py
+++ script.py
@@ -10,4 +10,13 @@ else:
 	print(app+" is an unknown application. exiting")
 	exit()
 print(cmd)
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+try:
+	p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+	p.wait()
+	(result, error) = p.communicate()
+except subprocess.CalledProcessError as e:
+	sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+
+print(result)
