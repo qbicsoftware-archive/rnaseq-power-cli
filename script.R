ae8ba7afa6085ef0c43c59006ccaf53d9b0f566f
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-11-09 14:56:11 +0100

    make arguments numeric

--- script.R
+++ script.R
@@ -4,10 +4,10 @@ args <- commandArgs(trailingOnly = TRUE)
 print(args)
 # trailingOnly=TRUE means that only your arguments are returned, check:
 # print(commandArgs(trailingOnly=FALSE))
-n <- args[1]
-rho <- args[2]
-lambda0 <- args[3]
-phi0 <- args[4]
-f <- args[5]
+n <- as.numeric(args[1])
+rho <- as.numeric(args[2])
+lambda0 <- as.numeric(args[3])
+phi0 <- as.numeric(args[4])
+f <- as.numeric(args[5])
 
 est_power(n=n, rho=rho, lambda0=lambda0, phi0=phi0,f=f)
