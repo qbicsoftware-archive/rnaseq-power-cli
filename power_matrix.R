ec352b741bc733c5211805715a40cd0809506038
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-11-10 15:33:49 +0100

    labels and file names

--- power_matrix.R
+++ power_matrix.R
@@ -2,11 +2,14 @@ library(RnaSeqSampleSize)
 
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
-m <- as.numeric(args[1])
-m1 <- as.numeric(args[2])
-rho <- as.numeric(args[3])
-phi0 <- as.numeric(args[4])
-f <- as.numeric(args[5])
+m <- as.numeric(args[1]) # number of genes
+m1 <- as.numeric(args[2]) # expected number of DE genes
+rho <- as.numeric(args[3]) # minimum detectable log fold change
+phi0 <- as.numeric(args[4]) # dispersion
+f <- as.numeric(args[5]) # FDR
 
 result<-optimize_parameter(fun=est_power,opt1="n", opt2="lambda0",opt1Value=c(5,10,15,20,25,30,45,50,55,60,65,70), opt2Value=c(1:5,10,20), m=m, m1=m1, rho=rho, phi0=phi0, alpha=f)
 print(result)
+pdf("power.pdf") 
+heatmap3(result, Colv = NA, Rowv = NA, xlab = "Samples per Group", ylab = "Average read count per gene", scale = "n", col = matlab::jet.colors(1000), cexCol = 1, cexRow = 1, lasCol = 1, lasRow = 1, main = "Power (sensitivity)")
+dev.off()
--- sample_size_matrix.R
+++ sample_size_matrix.R
@@ -2,11 +2,14 @@ library(RnaSeqSampleSize)
 
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
-m <- as.numeric(args[1])
-m1 <- as.numeric(args[2])
-phi0 <- as.numeric(args[3])
-f <- as.numeric(args[4])
-power <- as.numeric(args[5])
+m <- as.numeric(args[1]) # number of genes
+m1 <- as.numeric(args[2]) # expected number of DE genes
+phi0 <- as.numeric(args[3]) # dispersion
+f <- as.numeric(args[4]) # FDR
+power <- as.numeric(args[5]) # power (sensitivity)
 
 result<-optimize_parameter(fun=sample_size,opt1="rho", opt2="lambda0",opt1Value=c(1.1,2,3,4), opt2Value=c(1:5,10,20), power=power, m=m, m1=m1, phi0=phi0, f=f)
 print(result)
+pdf("sample_size.pdf")
+heatmap3(result, Colv = NA, Rowv = NA, xlab = "Log fold change", ylab = "Average read count per gene", scale = "n", col = matlab::jet.colors(1000), cexCol = 1, cexRow = 1, lasCol = 1, lasRow = 1, main = "Minimum sample size (per group)")
+dev.off()
