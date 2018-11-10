531e843babc199d17233e47adb074521cc3660ba
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-11-10 14:45:30 +0100

    fix paths

--- script.py
+++ script.py
@@ -3,9 +3,9 @@ import sys, subprocess
 app = sys.argv[1]
 arguments = sys.argv[2:]
 if app=="power":
-	cmd = ["Rscript", "power_matrix.R"] + arguments
+	cmd = ["Rscript", "/power_matrix.R"] + arguments
 elif app=="samples":
-	cmd = ["Rscript", "sample_size_matrix.R"] + arguments
+	cmd = ["Rscript", "/sample_size_matrix.R"] + arguments
 else:
 	print(app+" is an unknown application. exiting")
 	exit()
